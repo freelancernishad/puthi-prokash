b8509300386fa98395d05c5d70961dc1d197fdeb
Softlabltd.com <freelancernishad123@gmail.com>
2023-06-03 23:50:16 +0600

    update

--- resources/views/flipbook/index.blade.php
+++ resources/views/flipbook/index.blade.php
@@ -198,7 +198,7 @@ div.sideMenuItem {
               <div class="aspect-inner">
                 <div class="flipbook" id="flipbook" style="margin: 0 auto !important">
                     @foreach ($flipping_books as $item)
-                    <div class="page"><img class="sliderimage" src="placeholder.jpg" data-src="{{ asset($item->image) }}"  draggable="false" alt="" /></div>
+                    <div class="page"><img class="sliderimage" src="{{ asset($item->image) }}"  draggable="false" alt="" /></div>
                     @endforeach
                 </div>
               </div>

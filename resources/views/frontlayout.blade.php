50b0ca75471db01137cb69e5935469d75c366eee
Softlabltd.com <freelancernishad123@gmail.com>
2023-06-27 18:04:22 +0600

    update

--- resources/views/frontlayout.blade.php
+++ resources/views/frontlayout.blade.php
@@ -223,25 +223,5 @@ if ("{{ Auth::user() }}") {
  --}}
 
 
-
- <script>
-    window.fbAsyncInit = function() {
-      FB.init({
-        appId      : '1345041842759891',
-        xfbml      : true,
-        version    : 'v17.0'
-      });
-      FB.AppEvents.logPageView();
-    };
-
-    (function(d, s, id){
-       var js, fjs = d.getElementsByTagName(s)[0];
-       if (d.getElementById(id)) {return;}
-       js = d.createElement(s); js.id = id;
-       js.src = "https://connect.facebook.net/en_US/sdk.js";
-       fjs.parentNode.insertBefore(js, fjs);
-     }(document, 'script', 'facebook-jssdk'));
-  </script>
-
 </body>
 </html>

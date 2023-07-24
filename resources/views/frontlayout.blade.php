abe941d730f4601f73d22ae30fca22fa082504af
freelancernishad <freelancernishad123@gmail.com>
2023-07-25 01:18:32 +0600

    update

--- resources/views/frontlayout.blade.php
+++ resources/views/frontlayout.blade.php
@@ -96,7 +96,7 @@ ol#carousel-1___BV_indicators_ li {
 
 
 
-    <script src="{{ asset('js/frontend.js?ver=1.0.30') }}" async ></script>
+    <script src="{{ asset('js/frontend.js?ver=1.0.31') }}" async ></script>
 
 <script>
 
